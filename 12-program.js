093683518196688001270de76cd91d26778bf9c7
Curtis Bridges <curtis@curtisbridges.com>
2018-01-20 14:11:04 -0500

    solution to problem #12

---
+++ 12-program.js
@@ -0,0 +1,19 @@
+// Write an HTTP server that receives only POST requests and converts
+// incoming POST body characters to upper-case and returns it to the client.
+
+// Your server should listen on the port provided by the first argument to
+// your program.
+
+const http = require('http');
+
+const port = process.argv[2] || 2400;
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST') {
+    var map = require('through2-map')
+    req.pipe(map(function (chunk) {
+      return chunk.toString().toUpperCase();
+    })).pipe(res);
+  }
+});
+server.listen(port);
